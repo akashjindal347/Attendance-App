5df03b23717407e734ef6c0383185634f45a4a15
akash <akashjindal347@gmail.com>
2019-05-27 09:01:26 +0530

    init teacher ui

---
+++ lib/teacher/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'pages/main_page.dart';
+import 'pages/create_course.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return MaterialApp
+    (
+      title: 'Dashboard',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MainPage(),
+      routes: {
+        '/create': (BuildContext context)=>CreateCourse(),
+      },
+    );
+  }
+}
---
+++ lib/teacher/pages/main_page.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sparkline/flutter_sparkline.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+import 'shop_items_page.dart';
+
+class MainPage extends StatefulWidget
+{
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage>
+{
+  final List<List<double>> charts =
+  [
+    [0.0, 0.3, 0.7, 0.6, 0.55, 0.8, 1.2, 1.3, 1.35, 0.9, 1.5, 1.7, 1.8, 1.7, 1.2, 0.8, 1.9, 2.0, 2.2, 1.9, 2.2, 2.1, 2.0, 2.3, 2.4, 2.45, 2.6, 3.6, 2.6, 2.7, 2.9, 2.8, 3.4],
+    [0.0, 0.3, 0.7, 0.6, 0.55, 0.8, 1.2, 1.3, 1.35, 0.9, 1.5, 1.7, 1.8, 1.7, 1.2, 0.8, 1.9, 2.0, 2.2, 1.9, 2.2, 2.1, 2.0, 2.3, 2.4, 2.45, 2.6, 3.6, 2.6, 2.7, 2.9, 2.8, 3.4, 0.0, 0.3, 0.7, 0.6, 0.55, 0.8, 1.2, 1.3, 1.35, 0.9, 1.5, 1.7, 1.8, 1.7, 1.2, 0.8, 1.9, 2.0, 2.2, 1.9, 2.2, 2.1, 2.0, 2.3, 2.4, 2.45, 2.6, 3.6, 2.6, 2.7, 2.9, 2.8, 3.4,],
+    [0.0, 0.3, 0.7, 0.6, 0.55, 0.8, 1.2, 1.3, 1.35, 0.9, 1.5, 1.7, 1.8, 1.7, 1.2, 0.8, 1.9, 2.0, 2.2, 1.9, 2.2, 2.1, 2.0, 2.3, 2.4, 2.45, 2.6, 3.6, 2.6, 2.7, 2.9, 2.8, 3.4, 0.0, 0.3, 0.7, 0.6, 0.55, 0.8, 1.2, 1.3, 1.35, 0.9, 1.5, 1.7, 1.8, 1.7, 1.2, 0.8, 1.9, 2.0, 2.2, 1.9, 2.2, 2.1, 2.0, 2.3, 2.4, 2.45, 2.6, 3.6, 2.6, 2.7, 2.9, 2.8, 3.4, 0.0, 0.3, 0.7, 0.6, 0.55, 0.8, 1.2, 1.3, 1.35, 0.9, 1.5, 1.7, 1.8, 1.7, 1.2, 0.8, 1.9, 2.0, 2.2, 1.9, 2.2, 2.1, 2.0, 2.3, 2.4, 2.45, 2.6, 3.6, 2.6, 2.7, 2.9, 2.8, 3.4]
+  ];
+
+  static final List<String> chartDropdownItems = [ 'Last 7 days', 'Last month', 'Last year' ];
+  String actualDropdown = chartDropdownItems[0];
+  int actualChart = 0;
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold
+    (
+      appBar: AppBar
+      (
+        elevation: 2.0,
+        backgroundColor: Colors.white,
+        title: Text('Attendance Manager', style: TextStyle(color: Colors.black, fontWeight: FontWeight.w700, fontSize: 30.0)),
+        actions: <Widget>
+        [
+         
+        ],
+      ),
+      body: StaggeredGridView.count(
+        crossAxisCount: 2,
+        crossAxisSpacing: 12.0,
+        mainAxisSpacing: 12.0,
+        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 2.0),
+        children: <Widget>[
+          Container
+          (
+            margin: EdgeInsets.symmetric(vertical: 25.0, horizontal: 54.0),
+            child: Material
+            (
+              elevation: 8.0,
+              color: Colors.black,
+              borderRadius: BorderRadius.circular(32.0),
+              child: InkWell
+              (
+                onTap: ()=>Navigator.pushReplacementNamed(context, '/create'),
+                child: Padding
+                (
+                  padding: EdgeInsets.all(12.0),
+                  child: Row
+                  (
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>
+                    [
+                      Icon(Icons.add, color: Colors.white),
+                      Padding(padding: EdgeInsets.only(right: 16.0)),
+                      Text('ADD A COURSE', style: TextStyle(color: Colors.white))
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ),
+          
+         
+         
+          
+          _buildTile(
+            Padding
+            (
+              padding: const EdgeInsets.all(24.0),
+              child: Row
+              (
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>
+                [
+                  Column
+                  (
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>
+                    [
+                     
+                      Text('UTA002', style: TextStyle(color: Colors.black, fontWeight: FontWeight.w700, fontSize: 34.0)),
+                       Text('Mechanics', style: TextStyle(color: Colors.redAccent)),
+                    ],
+                  ),
+                  Material
+                  (
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(24.0),
+                    child: Center
+                    (
+                      child: Padding
+                      (
+                        padding: EdgeInsets.all(16.0),
+                        child: Icon(Icons.add, color: Colors.white, size: 30.0),
+                      )
+                    )
+                  )
+                ]
+              ),
+            ),
+            
+          )
+        ],
+        staggeredTiles: [
+          StaggeredTile.extent(2, 110.0),
+          StaggeredTile.extent(2, 110.0),
+          StaggeredTile.extent(2, 110.0),
+        ],
+      )
+    );
+  }
+
+  Widget _buildTile(Widget child, {Function() onTap}) {
+    return Material(
+      elevation: 14.0,
+      borderRadius: BorderRadius.circular(12.0),
+      shadowColor: Color(0x802196F3),
+      child: InkWell
+      (
+        // Do onTap() if it isn't null, otherwise do print()
+        onTap: onTap != null ? () => onTap() : () { print('Not set yet'); },
+        child: child
+      )
+    );
+  }
+}
